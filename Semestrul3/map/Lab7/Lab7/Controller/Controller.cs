7361271da2a20daba2a91100a2d5f9c9893c4d85
TudorPavaluca7 <42516844+TudorPavaluca7@users.noreply.github.com>
2019-02-25 15:43:09 +0200

    Add files via upload

---
+++ Semestrul3/map/Lab7/Lab7/Controller/Controller.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab7.Repository;
+using Lab7.Model;
+using Lab7.Model.Statements;
+namespace Lab7.Controller
+{
+    public class Controller
+    {
+        private IPrgRepository repo;
+
+        public Controller(IPrgRepository r)
+        {
+            this.repo = r;
+        }
+
+        public void executeOneStep()
+        {
+            PrgState p = repo.getCurrentPrgState();
+            Statement stmt = p.Stack.pop();
+
+            stmt.Execute(p);
+            Console.WriteLine(p);
+        }
+
+        public void executeAllSteps()
+        {
+            PrgState p = repo.getCurrentPrgState();
+            while (!p.Stack.IsEmpty())
+            {
+                this.executeOneStep();
+                repo.logPrgState();
+            }
+        }
+
+    }
+}
