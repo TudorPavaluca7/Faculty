a77d00688decffe251f0b82891a9e0fa69e2a310
TudorPavaluca7 <42516844+TudorPavaluca7@users.noreply.github.com>
2019-02-25 15:45:58 +0200

    Add files via upload

---
+++ Semestrul3/map/lab8/src/sample/SelectCtrl.java
@@ -0,0 +1,81 @@
+package sample;
+import Model.Expression.ArithmeticExpression;
+import Model.Expression.ConstExpression;
+import Model.Expression.ReadHeapExpression;
+import Model.Expression.VarExpression;
+import Model.Statement.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import Model.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SelectCtrl {
+
+    @FXML
+    private ListView<String> listView;
+
+    @FXML
+    private Button runProgramBTN;
+
+    public static IStmt statement;
+    private List<IStmt> StmtList = new ArrayList<IStmt>();
+
+    @FXML
+    public void initialize() {
+
+        IStmt ex1=new CompoundStatement(new AssignStatement("v",new ConstExpression(2)),new PrintStatement(new VarExpression("v")));
+        IStmt ex2=new CompoundStatement(new AssignStatement("a", new ArithmeticExpression('+', new ConstExpression(2), new
+                ArithmeticExpression('*', new ConstExpression(3), new ConstExpression(5)))),
+                new CompoundStatement(new AssignStatement("b", new ArithmeticExpression('+', new VarExpression("a"), new
+                        ConstExpression(1))), new PrintStatement(new VarExpression("b"))));
+
+        IStmt ex3=new CompoundStatement(new AssignStatement("v",new ConstExpression(10)),new CompoundStatement(
+                new NewStatement("a",new ConstExpression(22)),new CompoundStatement(
+                new ForkStatement(new CompoundStatement(new HeapWriteStatement("a",new ConstExpression(30)),new CompoundStatement(
+                        new AssignStatement("v",new ConstExpression(32)),new CompoundStatement(
+                        new PrintStatement(new VarExpression("v")),new PrintStatement(new ReadHeapExpression("a"))
+                )))),new CompoundStatement(
+                new PrintStatement(new VarExpression("v")),new PrintStatement(new ReadHeapExpression("a"))))));
+        StmtList.add(ex1);StmtList.add(ex2);StmtList.add(ex3);
+
+
+        ObservableList<String> list = FXCollections.observableArrayList();
+        for(IStmt i : StmtList)
+            list.add(""+i);
+
+        listView.setItems(list);
+
+        listView.getSelectionModel().selectIndices(0);
+
+    }
+    @FXML
+    public void buttonRun() throws IOException {
+        try{
+        statement = StmtList.get(listView.getSelectionModel().getSelectedIndex());
+
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        stage.setTitle("Program");
+        stage.setScene(new Scene(root, 800, 600));
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.show();}
+        catch(Exception e){
+                Alert a = new Alert(Alert.AlertType.ERROR, e.getMessage());
+                a.show();
+            }
+    }
+
+
+
+}
